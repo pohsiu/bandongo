45ef8771763fc0d174056c7f82122e6e5b8ea4b9
Louis <louis.hsiao@d8ai.com>
2020-01-10 14:22:40 +0800

    heroku: setting chagne

--- bandon/settings.py
+++ bandon/settings.py
@@ -25,6 +25,8 @@ except:
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+import django_heroku
+django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -134,3 +136,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
